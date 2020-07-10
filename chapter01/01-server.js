f9229b9e52c125aa321dc5a4ee74cae2e363e223
Rabra <rabira.hierpa@gmail.com>
2020-07-10 18:08:08 +0300

    Added content type of JSON

--- chapter01/01-server.js
+++ chapter01/01-server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const port = process.env.PORT || 1337;
 const server = http.createServer(function (req, res) {
-	res.end('Hello');
+	res.setHeader('Content-Type', 'application/json');
+	res.end(JSON.stringify({ text: 'Hello', numbers: [1, 2, 3] }));
 });
 server.listen(port);
 console.log(`Server listening on port ${port}`);
