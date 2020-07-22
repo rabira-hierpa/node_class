04385ebb19913305b6b76496c890bc69b83d8f45
Rabra <rabira.hierpa@gmail.com>
2020-07-22 16:01:54 +0300

    Converted the node app to epxress

--- chapter01/04-express.js
+++ chapter01/04-express.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const fs = require('fs');
+const express = require('express');
 const port = process.env.PORT || 1337;
 let textResponse = 0;
 // respond with plain text
@@ -12,8 +12,9 @@ function respondText(req, res) {
 // respond with JSON
 function respondJSON(req, res) {
 	console.log('1-respondJSON served at ' + Date.now());
-	res.setHeader('Content-Type', 'appliction/json');
-	res.end(JSON.stringify({ text: 'Hello', numbers: [1, 2, 3] }));
+	// res.setHeader('Content-Type', 'appliction/json');
+	// res.end(JSON.stringify({ text: 'Hello', numbers: [1, 2, 3] })); # core http module
+	res.json({ text: 'Hello', numbers: [13, 45, 14], functions: 'express()' });
 }
 // respond not found (404)
 function respondNotFound(req, res) {
@@ -23,41 +24,48 @@ function respondNotFound(req, res) {
 }
 // respons for echo url
 function respondEcho(req, res) {
-	const { input = '' } = qureystring.parse(
-		req.url.split('?').slice(1).join('')
-	);
+	// core http()
+	// const { input = '' } = qureystring.parse(
+	// 	req.url.split('?').slice(1).join('')
+	// );
+	// res.setHeader('Content-Type', 'application/json');
+	// res.end(
+	// 	JSON.stringify({
+	// 		normal: input,
+	// 		shouty: input.toUpperCase(),
+	// 		charachterCount: input.length + ' characters',
+	// 		backwards: input.split('').reverse().join(''),
+	// 	})
+	// );
+	// express()
+	const { input = '' } = req.query;
+	res.json({
+		normal: input,
+		shouty: input.toUpperCase(),
+		charachterCount: input.length,
+		backwards: input.split('').reverse().join(''),
+	});
 
-	res.setHeader('Content-Type', 'application/json');
-	res.end(
-		JSON.stringify({
-			normal: input,
-			shouty: input.toUpperCase(),
-			charachterCount: input.length,
-			backwards: input.split('').reverse().join(''),
-		})
-	);
 	console.log('Echo response served at ' + Date.now());
 }
 // File serving
 function respondStatic(req, res) {
 	// simple translation of req.url to local dir strutucture
-	const filename = `${__dirname}/public${req.url.split('/static')[1]}`;
+	// const filename = `${__dirname}/public${req.url.split('/static')[1]}`; // #core http()
 	// read file from the local dir and send it to the browser
 	// createReadStream ==> creates a Stream object for the given filename
 	// on() ==> listens for error
 	// pipe() ==> connects to the response object
+	const filename = `${__dirname}/public/${req.params[0]}`;
 	fs.createReadStream(filename)
 		.on('error', () => respondNotFound(req, res))
 		.pipe(res);
 }
 // Dynamic serving
-const server = http.createServer(function (req, res) {
-	if (req.url === '/') return respondText(req, res);
-	if (req.url === '/json') return respondJSON(req, res);
-	if (req.url.match(/^\/echo/)) return respondEcho(req, res);
-	if (req.url.match(/^\/static/)) return respondStatic(req, res);
-	respondNotFound(req, res);
-});
+const app = express();
 
-server.listen(port);
-console.log(`Server listening on port ${port}`);
+app.get('/', respondText);
+app.get('/json', respondJSON);
+app.get('/echo', respondEcho);
+app.get('/static/*', respondStatic);
+app.listen(port, () => console.log(`Sever running on port ${port}`));
